4d7e52ec16ba73086aa5de2d2fc80a85309fcc01
Eiasin Prodhan <eiasinprodhan@gmail.com>
2026-04-13 19:58:28 +0600

    Changed

--- auth/src/main/java/com/eiasinprodhan/auth/util/JwtUtil.java
+++ auth/src/main/java/com/eiasinprodhan/auth/util/JwtUtil.java
@@ -1,11 +1,11 @@
 package com.eiasinprodhan.auth.util;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
@@ -15,24 +15,34 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    public static final String SECRET = "FDGSDFGSTR4ET654GSD5G4DFS65G4DFSG54R65ET4EWR65T4DGD";
 
+    public static final String SECRET_KEY = "ASHHDFHSOIUEUBDIFBUIEWGFVSDVFIWWEE487536DGKFHGHDSGFHKSDGFUEFUEVCUKEUFUDVCVDHSVHSDVHF";
 
-    public String generateJwtToken(String username, String password) {
-        return  Jwts.builder()
+    public String generateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", "satish@gmail.com");
+
+        return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 3600 * 1000))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))
+                .claims(claims)
                 .signWith(getKey())
                 .compact();
     }
 
     private Key getKey() {
-        byte[] bytes = Base64.getDecoder().decode(SECRET);
+        byte[] bytes = Base64.getDecoder().decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(bytes);
     }
 
-    private Claims getClaimsFromToken(String token) {
-        Jwts.parser().verifyWith(Sec)
+    private Claims getClaims(String token){
+        return Jwts.parser().verifyWith((SecretKey) getKey())
+                .build().parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public Date extractExpiration(String token){
+        return getClaims(token).getExpiration();
     }
 }
